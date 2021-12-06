14784d811b06b0e887ebf8ce835302ead28e83e1
Gustavo Galli <gustavosgalli@gmail.com>
2021-12-06 10:37:29 -0300

    @gustavogalli [Task: Documentação da classe Usuario]

---
+++ src/main/java/com/projetointegradorg3/redeSocial/model/Usuario.java
@@ -0,0 +1,32 @@
+package com.projetointegradorg3.redeSocial.model;
+
+/**
+ * <p>
+ * - id: representa a coluna de id do usuário. Essa coluna é gerada automaticamente e possui AUTO_INCREMENT.
+ * </p>
+ * 
+ * <p>
+ * - nome: representa o nome completo do usuário;
+ * </p>
+ * 
+ * <p>
+ * - email: representa o email que será utilizado para acessar;
+ * </p>
+ * 
+ * <p>
+ * - senha: representa a senha de acesso do usuário;
+ * </p>
+ * 
+ * @author Caio Gabriel
+ * @author Gustavo Galli
+ * @author João Almeida
+ * @author Raphaella Guerra
+ * @since 1.0
+ *
+ */
+
+public class Usuario {
+
+	
+
+}
---
+++ src/test/java/com/projetointegradorg3/redeSocial/RedeSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetointegradorg3.redeSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedeSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
