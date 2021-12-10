621a896cca7065a43e8a785a88834e2c476644b8
João Almeida <78218571+joao295@users.noreply.github.com>
2021-12-10 11:37:43 -0300

    @joao295 [Task: Criando Relacionamento entre tabelas]

--- src/main/java/com/projetointegradorg3/redeSocial/model/Tema.java
+++ src/main/java/com/projetointegradorg3/redeSocial/model/Tema.java
@@ -1,75 +1,72 @@
 package com.projetointegradorg3.redeSocial.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
-@Table (name = "tema")
+@Table(name = "tema")
 public class Tema {
 
-@Id
-@GeneratedValue(strategy = GenerationType.IDENTITY)
-private long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 
-@NotBlank
-@Size(max = 15)
-private String curso; 
+	@NotBlank
+	@Size(max = 15)
+	private String curso;
 
-@NotNull
-private int pontosAprender;
+	@NotNull
+	private int pontosAprender;
 
+	@NotNull
+	private int pontosEnsinar;
 
-@NotNull
-private int pontosEnsinar;
+	@OneToMany(mappedBy = "postagem", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("pontosDeExperiencia")
+	private List<Postagem> postagem;
 
+	public long getId() {
+		return id;
+	}
 
-public long getId() {
-	return id;
-}
+	public void setId(long id) {
+		this.id = id;
+	}
 
+	public String getCurso() {
+		return curso;
+	}
 
-public void setId(long id) {
-	this.id = id;
-}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
 
+	public int getPontosAprender() {
+		return pontosAprender;
+	}
 
-public String getCurso() {
-	return curso;
-}
+	public void setPontosAprender(int pontosAprender) {
+		this.pontosAprender = pontosAprender;
+	}
 
+	public int getPontosEnsinar() {
+		return pontosEnsinar;
+	}
 
-public void setCurso(String curso) {
-	this.curso = curso;
-}
-
-
-public int getPontosAprender() {
-	return pontosAprender;
-}
-
-
-public void setPontosAprender(int pontosAprender) {
-	this.pontosAprender = pontosAprender;
-}
-
-
-public int getPontosEnsinar() {
-	return pontosEnsinar;
-}
-
-
-public void setPontosEnsinar(int pontosEnsinar) {
-	this.pontosEnsinar = pontosEnsinar;
-}
-	
-
-
-
+	public void setPontosEnsinar(int pontosEnsinar) {
+		this.pontosEnsinar = pontosEnsinar;
+	}
 
 }
