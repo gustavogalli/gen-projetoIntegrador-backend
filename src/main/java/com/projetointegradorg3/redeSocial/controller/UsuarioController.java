9660e8f27d0708dcf3e04dda0a4d81a595925a9c
Gustavo Galli <gustavosgalli@gmail.com>
2021-12-10 10:52:55 -0300

    @gustavogalli [Task: Adicionando a UsuarioController]

---
+++ src/main/java/com/projetointegradorg3/redeSocial/controller/UsuarioController.java
@@ -0,0 +1,46 @@
+package com.projetointegradorg3.redeSocial.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetointegradorg3.redeSocial.model.Usuario;
+
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin("*")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	@GetMapping("/all")
+	public List<Usuario> getAll(){
+		return repository.findAll();
+	}
+	
+	@PostMapping("/save")
+	public Usuario post (@RequestBody Usuario usuario) {
+		return repository.save(usuario);
+	}
+	
+	@PutMapping("/update")
+	public Usuario put (@RequestBody Usuario usuario) {
+		return repository.save(usuario);
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public Usuario delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+
+}
