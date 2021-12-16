a862cb3700c6ad87244988ad17134341733137e9
Gustavo Galli <gustavosgalli@gmail.com>
2021-12-16 14:45:18 -0300

    @gustavogalli [Task: adicionando classe UserDetailsImplements]

--- src/main/java/com/projetointegradorg3/redeSocial/security/UserDetailsImplements.java
+++ src/main/java/com/projetointegradorg3/redeSocial/security/UserDetailsImplements.java
@@ -1,5 +1,59 @@
 package com.projetointegradorg3.redeSocial.security;
 
-public class UserDetailsImplements {
+import java.util.Collection;
+import java.util.List;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.projetointegradorg3.redeSocial.model.Usuario;
+
+public class UserDetailsImplements implements UserDetails {
+	private static final long serialVersionUID = 1L;
+	private String userName;
+	private String password;
+	private List<GrantedAuthority> authorities;
+
+	public UserDetailsImplements(Usuario user) {
+		this.userName = user.getEmail();
+		this.password = user.getSenha();
+	}
+
+	public UserDetailsImplements() {
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
