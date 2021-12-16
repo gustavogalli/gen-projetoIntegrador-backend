8506c05b932d663b1147bd0a0cdd1b66d5dcb449
João Almeida <78218571+joao295@users.noreply.github.com>
2021-12-16 14:53:49 -0300

    @joao295 [Task: Ajustando Usuario Repository]

--- src/main/java/com/projetointegradorg3/redeSocial/repository/UsuarioRepository.java
+++ src/main/java/com/projetointegradorg3/redeSocial/repository/UsuarioRepository.java
@@ -2,6 +2,7 @@ package com.projetointegradorg3.redeSocial.repository;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,9 @@ import com.projetointegradorg3.redeSocial.model.Usuario;
 
 @Repository
 public interface UsuarioRepository  extends JpaRepository<Usuario, Long>{
+	
+	
+	public Optional<Usuario> findByUsuario(String usuario);
 	
 	public List<Usuario>findByEmailContainingIgnoreCase(String email);
 
