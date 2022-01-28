4ce41fc38488aa6b2f67d1ea29e96389694a2e9c
Gustavo Galli <75743915+gustavogalli@users.noreply.github.com>
2022-01-28 10:22:26 -0300

    Update BasicSecurityConfiguration.java

--- src/main/java/com/projetointegradorg3/redeSocial/security/BasicSecurityConfiguration.java
+++ src/main/java/com/projetointegradorg3/redeSocial/security/BasicSecurityConfiguration.java
@@ -30,9 +30,19 @@ public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	}
 
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/users/logar").permitAll().antMatchers("/users/cadastrar").permitAll()
-				.anyRequest().authenticated().and().httpBasic().and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and().csrf().disable();
-	}
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests()
+        .antMatchers("/**").permitAll()
+        .antMatchers("/usuarios/logar").permitAll()
+        .antMatchers("/usuarios/cadastrar").permitAll()
+        .antMatchers(HttpMethod.POST ,"/postagens").permitAll()
+        .antMatchers(HttpMethod.POST ,"/tema").permitAll()
+        .antMatchers(HttpMethod.GET ,"/postagens").permitAll()
+        .antMatchers(HttpMethod.GET ,"/tema").permitAll()
+        .anyRequest().authenticated()
+        .and().httpBasic()
+        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and().cors()
+        .and().csrf().disable();
+    }
 }
