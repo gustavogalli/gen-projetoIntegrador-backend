f27ccc3e4608b52304b347e52b38cb52a222e2f9
João Almeida <78218571+joao295@users.noreply.github.com>
2022-01-31 09:04:24 -0300

    @joao295 [task: remove unused imports]

--- src/main/java/com/projetointegradorg3/redeSocial/security/BasicSecurityConfiguration.java
+++ src/main/java/com/projetointegradorg3/redeSocial/security/BasicSecurityConfiguration.java
@@ -3,8 +3,6 @@ package com.projetointegradorg3.redeSocial.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
