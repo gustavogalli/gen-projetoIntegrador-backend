4225f75a73186ef8c971291208324abab8885669
João Almeida <78218571+joao295@users.noreply.github.com>
2022-01-31 09:05:25 -0300

    @joao295 [task: change position of senha]

--- src/main/java/com/projetointegradorg3/redeSocial/service/UsuarioService.java
+++ src/main/java/com/projetointegradorg3/redeSocial/service/UsuarioService.java
@@ -48,10 +48,14 @@ public class UsuarioService {
 				usuarioLogin.get().setId(usuario.get().getId());
 				usuarioLogin.get().setNome(usuario.get().getNome());
 				usuarioLogin.get().setFoto(usuario.get().getFoto());
-				usuarioLogin.get().setSenha(usuario.get().getSenha());
-				usuarioLogin.get().setTipo(usuario.get().getTipo());
 				usuarioLogin.get().setToken(generatorBasicToken(usuarioLogin.get().getUsuario(), usuarioLogin.get().getSenha()));
+				usuarioLogin.get().setSenha(usuario.get().getSenha());
+			
+				usuarioLogin.get().setTipo(usuario.get().getTipo());
+			
 				return usuarioLogin;
+				
+				
 			}
 		}
 		throw new ResponseStatusException(
